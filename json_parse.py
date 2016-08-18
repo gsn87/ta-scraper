a1049bb1ca80ba72d26cb7a342d2560d12097676
gsn87 <guillaume.simonin@gmail.com>
2016-08-18 18:36:07 +0200

    counting mails

--- json_parse.py
+++ json_parse.py
@@ -3,7 +3,7 @@ __author__ = 'gsn'
 import json
 from pprint import pprint
 
-with open('./output/paris_restaurant.json') as data_file:
+with open('./output/all.json') as data_file:
 	data = json.load(data_file)
 
 # for restaurant in data:
@@ -14,4 +14,4 @@ for i in range(len(data)):
 		email.append(restaurant_email)
 		pprint(restaurant_email)
 
-pprint(len(email))
+pprint(str(len(email)) + '/' + str(len(data)))
