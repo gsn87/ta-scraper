b10728390d11f1f9b3ec42588e24374e14fc8c21
gsn87 <guilllaume.simonin@gmail.com>
2016-09-04 22:06:16 +0200

    parse paris data

--- json_parse.py
+++ json_parse.py
@@ -1,6 +1,7 @@
 __author__ = 'gsn'
 
 import json
+import csv
 from pprint import pprint
 
 with open('./output/all.json') as data_file:
@@ -8,10 +9,25 @@ with open('./output/all.json') as data_file:
 
 # for restaurant in data:
 email = []
-for i in range(len(data)):
-	restaurant_email = data[i]["address"]["email"]
-	if restaurant_email != 'none':
-		email.append(restaurant_email)
-		pprint(restaurant_email)
+
+with open('./output/paris.csv', 'wb') as csvfile:
+	print('name \t email \t phone \t street \t cp \t city')
+	writer = csv.writer(csvfile, delimiter='\t')
+	for i in range(len(data)):
+		# print data[i]["address"]
+		name = data[i]["name"]
+		# phone = data[i]["address"]["phone"]
+		mail = data[i]["address"]["email"].encode('utf-8')
+		street = data[i]["address"]["street"]
+		# cp = data[i]["address"]["postal_code"]
+		try:
+			city = data[i]["address"]["locality"]
+		except:
+			pass
+		if mail != 'none' and city == 'Paris':
+			email.append(mail)
+			# print('%s \t %s' % (name, mail))
+			print name, mail
+			writer.writerow([name, mail])
 
 pprint(str(len(email)) + '/' + str(len(data)))
